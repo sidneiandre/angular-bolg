d63b7a4eee26f8d994d5f1d2027a6989773ad049
sidneiandre <sidneiandre1988@gmail.com>
2024-02-13 16:24:26 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Nova Temporada Smallville",
+    "description": "Decima Temporada vem com muitas novidades e promete causar muitas emocões e muito suspence.",
+    "photoCover": "https://seriemaniacos.tv/wp-content/uploads/2011/01/Smallville-season-10.jpeg"
+  },
+  {
+    "id":"2",
+    "title": "A Nova Temporada vai deixar você de queixo caido",
+    "description": "ola ola ola",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj5pbjeBV4YzunYhRqosGJ4kcJyEzfz5inyQ&usqp=CAU"
+
+  }
+]
+
+
